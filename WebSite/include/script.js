434df400d5e51bff7e34e9cbe207b4597177bf19
gudmunduroh <gudmundurha203@nemi.tskoli.is>
2017-04-28 13:32:54 +0000

    css og js

--- WebSite/include/script.js
+++ WebSite/include/script.js
@@ -6,16 +6,16 @@ class Menu {
     static open()
     {
         $("#menuOption1").css({
-            transition: "background-color 0.3s linear, opacity 0.4s ease-out 0.5s, transform 0.4s ease-out 0.5s"
+            transition: "background-color 0.3s linear, opacity 0.4s ease-out 0.3s, transform 0.4s ease-out 0.3s"
         });
         $("#menuOption2").css({
-            transition: "background-color 0.3s linear, opacity 0.4s ease-out 0.6s, transform 0.4s ease-out 0.6s"
+            transition: "background-color 0.3s linear, opacity 0.4s ease-out 0.4s, transform 0.4s ease-out 0.4s"
         });
         $("#menuOption3").css({
-            transition: "background-color 0.3s linear, opacity 0.4s ease-out 0.7s, transform 0.4s ease-out 0.7s"
+            transition: "background-color 0.3s linear, opacity 0.4s ease-out 0.5s, transform 0.4s ease-out 0.5s"
         });
         $("#menuOption4").css({
-            transition: "background-color 0.3s linear, opacity 0.4s ease-out 0.8s, transform 0.4s ease-out 0.8s"
+            transition: "background-color 0.3s linear, opacity 0.4s ease-out 0.6s, transform 0.4s ease-out 0.6s"
         });
         $("#navBar").addClass("showNavBar");
         $("#mobileNav").addClass("menuOpen");
